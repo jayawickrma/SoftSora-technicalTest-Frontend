ff66e8d474cee1e8063e786db0e33881ff6a8f8e
jayawickrama <nishantharaka2004@gmail.com>
2025-05-31 22:02:35 +0530

    created TaskCardComponent.tsx ,TaskPage ,Task Model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-cimport './App.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {TaskPage} from "./Pages/TaskPage.tsx";
+
 
 function App() {
 
   return (
     <>
+        <Router>
+            <Routes>
+                <Route path="/" element={<TaskPage/>}  />
+            </Routes>
+        </Router>
     </>
   )
 }
---
+++ src/Pages/TaskPage.tsx
@@ -0,0 +1,7 @@
+export function TaskPage(){
+    return(
+        <>
+             <h1>View all tasks</h1>
+        </>
+    )
+}

ba264290f0f3ae3d86cee832864508f2c3414b05
jayawickrama <nishantharaka2004@gmail.com>
2025-06-02 20:07:47 +0530

    started to adding axios

--- src/Slices/UserSlice.ts
+++ src/Slices/UserSlice.ts
@@ -1,6 +1,18 @@
-// import {createSlice} from "@reduxjs/toolkit";
-//
-// const UserSlice =createSlice({
-//
-// })
-// export default UserSlice.reducer;
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import type {UserModel} from "../Model/UserModel.ts";
+
+
+export const register =createAsyncThunk (
+    "auth/signIn",
+    async(user:UserModel)=>{
+        try{
+
+        }
+}
+)
+
+
+const UserSlice =createSlice({
+
+})
+export default UserSlice.reducer;
